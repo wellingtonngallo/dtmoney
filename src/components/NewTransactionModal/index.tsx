0637d7766f96bad67c225923e1e0c93269173cff
Wellington Gallo <wellinton.gallo77@gmail.com>
2021-03-17 23:36:26 -0300

    estiliza botao

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
-import { Container } from './styles';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import { Container, TransactionTypeContainer } from './styles';
 
 Modal.setAppElement('#root');
 
@@ -36,6 +38,20 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
           placeholder="Valor"
         />
 
+        <TransactionTypeContainer>
+          <button
+            type="button"
+          >
+            <img src={incomeImg} alt="Entrada"></img>
+            <span>Entrada</span>
+          </button>
+          <button
+            type="button"
+          >
+            <img src={outcomeImg} alt="Saida"></img>
+            <span>Saida</span>
+          </button>
+        </TransactionTypeContainer>
         <input
           placeholder="Categoria"
         />
